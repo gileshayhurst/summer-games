b74795cd00f22be71b458c88608396c37c68797c
gileshayhurst <gileshayhurst@icloud.com>
2026-07-01 16:11:46 -0700

    chore: exclude worktree directories from jest test discovery

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.worktrees/', '<rootDir>/.claude/worktrees/'],
 }
 
 export default createJestConfig(config)
